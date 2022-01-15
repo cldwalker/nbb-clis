63c1991c918c2cad12e8d9af7a875185e68f0825
Gabriel Horner <gabriel.horner@gmail.com>
2022-01-15 10:51:15 -0500

    First pass at tests and two lint jobs

---
+++ test/cldwalker/nbb_clis/logseq_roundtrip_test.cljs
@@ -0,0 +1,26 @@
+(ns cldwalker.nbb-clis.logseq-roundtrip-test
+  (:require [clojure.test :as t :refer [deftest is]]
+            ["fs" :as fs]
+            [cldwalker.nbb-clis.util :as util]))
+
+(defn- resource [path]
+  (str "test/cldwalker/nbb_clis/resources/" path))
+
+(deftest roundtrip-basic-test
+  (util/sh ["bin/logseq-roundtrip"
+            (resource "basic.md")
+            (resource "basic2.md")] {})
+  (is (= (str (fs/readFileSync (resource "basic.md")))
+         (str (fs/readFileSync (resource "basic2.md")))))
+  ;; Clean up
+  (fs/unlinkSync (resource "basic2.md")))
+
+;; Commented out b/c of a bug with blocks losing one property
+#_(deftest roundtrip-advanced-test
+  (util/sh ["bin/logseq-roundtrip"
+            (resource "advanced.md")
+            (resource "advanced2.md")] {})
+  (is (= (str (fs/readFileSync (resource "advanced.md")))
+         (str (fs/readFileSync (resource "advanced2.md")))))
+  ;; Clean up
+  (fs/unlinkSync (resource "advanced2.md")))
---
+++ test/cldwalker/nbb_clis/resources/advanced.md
@@ -0,0 +1,9 @@
+- apple #some-tag
+	- banana #work #blarg
+	  some:: property
+	  another:: one
+		- fig
+		  collapsed:: true
+		  DEADLINE <2021-04-30 Fri>
+		- TODO grape
+- pear
